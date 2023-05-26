efdde82db488f0571c1272e92133730f92ce76ef
Weiwei Zhuo <115064974+zhuoww@users.noreply.github.com>
2023-05-26 15:31:38 -0700

    Update test_octoprime_tire.py

--- lyft-task4-TDD/test/test_tire/test_octoprime_tire.py
+++ lyft-task4-TDD/test/test_tire/test_octoprime_tire.py
@@ -3,7 +3,7 @@ import unittest
 from tire.octoprime_tire import OctoprimeTire
 
 
-class TestCarriganTire(unittest.TestLoader):
+class TestOctoprimeTire(unittest.TestLoader):
     def test_needs_service_true(self):
         tire_wear_array = [1.4,0.9,0.6,1.2]
         tire = OctoprimeTire(tire_wear_array)
